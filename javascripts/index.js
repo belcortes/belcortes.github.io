32d6bd7e7d3fa48dda94a0ae0fee2aa220b3632e
Isabel Cortes <belcortes@gmail.com>
2017-09-18 14:59:42 -0700

    revamping

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,20 +1,23 @@
+function contactForm(){
 
+}
 
 $(function() {
-  var diag_contact = $('<div title="Email me at:" id="contact-info"></div>');
+  // var diag_contact = $('<div title="Email me at:" id="contact-info"></div>');
   
-  diag_contact.dialog({
-      autoOpen: false,
-      modal: true,
-      show: "fadeIn",
-      width: '30%',
+  // diag_contact.dialog({
+  //     autoOpen: false,
+  //     modal: true,
+  //     show: "fadeIn",
+  //     width: '30%',
 
-  });
+  // });
   
   $('#box7').click(function() {
-      $('#contact-info').text("belcortes@gmail.com");
-      diag_contact.dialog("open");
-      $('body').css('background-color', '#3b3b3b')
+    $('#main-page').fadeOut(1000, function(){$('#contact').fadeIn();});
+      // $('#contact-info').text("belcortes@gmail.com");
+      // diag_contact.dialog("open");
+      // $('body').css('background-color', '#3b3b3b')
   });
   $('.ui-button').on('click', function(){
     $('body').css('background-color', 'white')
